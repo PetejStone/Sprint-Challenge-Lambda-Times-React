d3d688d2bbe5936e96db42d9ac7e2346f21bb24e
PetejStone <petejstoney@gmail.com>
2019-05-03 14:33:35 -0700

    brought back filterCards function and got it working

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -28,40 +28,19 @@ export default class Content extends Component {
 
   
 
-  componentDidUpdate() {
-    if (this.state.selected !== this.state.selected) {
-      this.setState({
-        selected: 'all',
-        tabs: tabData,
-        cards: cardData
-      })
-    }
-  }
+  
+  
 
   changeSelected = e => {
     // this function should take in the tab and update the state with the new tab
-     const newCardsArray = cardData.filter(card => card.tab === e.target.textContent.toLowerCase())
+  
      const newTab = e.target.textContent.toLowerCase()
-     console.log(newCardsArray)
     
-     if (newTab === 'all') {
       this.setState({
-        selected: newTab,
-        cards: cardData
+        selected: newTab
+       
       })
-     } else {
-     this.setState({
-      selected: newTab,
-      cards: newCardsArray
-    })
-     }
-    
-    
-    
-   
-    //this.setState({ cards: newCardsArray
   };
-
   filterCards = () => {
     /* Right now this function only returns the cards on state.
       We're going to make this function more dynamic
@@ -69,32 +48,20 @@ export default class Content extends Component {
       
       Notice that we're passing this function to our <Cards /> component below.
       This function returns an array of cards, so we can just pass it down as such.
-
       Your algorithim for the logic here is as follows: 
         - if the selected tab is 'all' it should return all 
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    //console.log('step1')
-    //console.log(this.state.selected)
-    const newCardsArray = this.state.cards.filter(card => card.tab === this.state.selected)
-    if (this.state.selected === 'all') {
-      this.setState({data: cardData})
-    } else {
-      this.setState({cards: newCardsArray})
-    }
+   
       
   if (this.state.selected === 'all') {
     return this.state.cards
   } else  {
     console.log('testing')
-    return this.state.cards
+    return this.state.cards.filter(card => card.tab === this.state.selected)
     
-  }
-  
-    
-  //this.setState({cards: cardData})
-  return this.state.cards
+    }
   };
 
   render() {
@@ -107,7 +74,7 @@ export default class Content extends Component {
         */}
         
         <Tabs tabs={this.state.tabs} changeSelected={this.changeSelected} />
-        <Cards cards={this.state.cards} />
+        <Cards cards={this.filterCards()} />
       </div>
     );
   }
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+// const Button = styled.button `
+//   background: transparent;
+//   border-radius: 3px;
+//   border: 2px solid palevioletred;
+//   color: palevioletred;
+//   margin: 0 1em;
+//   padding: 0.25em 1em;
+
+// ${props => props.primary && css ` background: palevioletred; color: white;`};
+//  `;
 
 const TopBar = () => {
   return (
